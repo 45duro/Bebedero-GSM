d0a0daf8983e1a4b10f2d70c42492c1015f899f2
Jonathan Gonzalez <jonathangc.awt@gmail.com>
2020-12-05 22:37:24 -0500

    interfaz funcionando sin problemas

--- bebedero/bebedero.ino
+++ bebedero/bebedero.ino
@@ -4,11 +4,7 @@
 //Create software serial object to communicate with SIM800L
 SoftwareSerial mySerial(8, 7); //SIM800L Tx & Rx is connected to Arduino #8 & #7
 
-
-String numeroApoyo;
-String servidor;
-String datoMensaje;
-String MensajeRecibido;
+String datoMensaje = "";
 
 // ****************************
 //                            *
@@ -16,24 +12,28 @@ String MensajeRecibido;
 //                            *
 // ****************************
 struct Settings{
-  String NumeroTelefonicoUsuario;
-  String NumeroTelefonicoServer;
+  char NumeroTelefonicoUsuario[11];
+  char NumeroTelefonicoServer[11];
 
   int levelMAX, levelMIN;
 
-} Configuracion;
+};
+
+Settings Configuracion;
 
 void setup() {
   //Delay para evitar las fluctuaciones de corriente que evitan que funcione correctamente
-  delay(3000);
+  delay(4000);
   //Begin serial communication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(9600);
-  
+  LecturaDeEEPROM();
+  //GuardarEnEEPROM();
+
   //Begin serial communication with Arduino and SIM800L
   mySerial.begin(9600);
   
   Serial.println("Initializing...");
-  LecturaDeEEPROM();
+  
 
 
   mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
@@ -57,8 +57,8 @@ void setup() {
   mySerial.println("AT+CNMI=2,2"); // Configuring TEXT mode
   updateSerial();
   
-  /*
-  String mensage = "AT+CMGS=\"" + Configuracion.NumeroTelefonicoUsuario + "\"";
+  Serial.print("numero del server "); Serial.println(Configuracion.NumeroTelefonicoUsuario); 
+  String mensage = "AT+CMGS=\"" + String(Configuracion.NumeroTelefonicoUsuario) + "\"";
   mySerial.println(mensage);//change ZZ with country code and xxxxxxxxxxx with phone number to sms
   updateSerial();
   
@@ -68,7 +68,7 @@ void setup() {
   
   mySerial.write(26);
 
-  */
+
 
 }
 
@@ -92,25 +92,25 @@ void updateSerial()
       datoMensaje.trim();
       Serial.println (datoMensaje);//Forward what Software Serial received to Serial Port
 
-      int inicio=0, fin;
+      byte inicio=0, fin;
       
       if(datoMensaje.indexOf("**")>0)
       {  
         inicio = datoMensaje.indexOf("**");
       }
-      if(datoMensaje.indexOf("*#")>0)
+      else if(datoMensaje.indexOf("*#")>0)
       {  
         inicio = datoMensaje.indexOf("*#");
       }
-      if(datoMensaje.indexOf("*?")>0)
+      else if(datoMensaje.indexOf("*?")>0)
       {  
         inicio = datoMensaje.indexOf("*?");
       }
-      if(datoMensaje.indexOf("*$")>0)
+      else if(datoMensaje.indexOf("*$")>0)
       {  
         inicio = datoMensaje.indexOf("*$");
       }
-      if(datoMensaje.indexOf("*%?")>0)
+      else if(datoMensaje.indexOf("*%?")>0)
       {  
         inicio = datoMensaje.indexOf("*%?");
       }
@@ -118,20 +118,28 @@ void updateSerial()
       
 
       if(datoMensaje.startsWith("**", inicio) && datoMensaje.endsWith("*!")){
+        String x =  datoMensaje.substring(inicio + 2, fin);
+        Serial.print("extraccion: "); Serial.println(x); 
+        x.toCharArray(Configuracion.NumeroTelefonicoUsuario, 33);
         
-        Configuracion.NumeroTelefonicoUsuario = datoMensaje.substring(inicio + 2, fin);
-        Serial.println("Cambiar el numero de usuario a: " + Configuracion.NumeroTelefonicoUsuario);
+        Serial.print("Cambiar el numero de usuario a: ");
+        Serial.write(Configuracion.NumeroTelefonicoUsuario);
         GuardarEnEEPROM();
         
       }
+      
       else if(datoMensaje.startsWith("*#", inicio) && datoMensaje.endsWith("*!")){
         
-        Configuracion.NumeroTelefonicoServer = datoMensaje.substring(inicio + 2, fin);
-        Serial.println("Cambiar el numero de server a: " + Configuracion.NumeroTelefonicoServer);
+        String x =  datoMensaje.substring(inicio + 2, fin);
+        Serial.print("extraccion: "); Serial.println(x); 
+        x.toCharArray(Configuracion.NumeroTelefonicoServer, 33);
+        
+        Serial.print("Cambiar el numero de usuario a: ");
+        Serial.write(Configuracion.NumeroTelefonicoServer);
         GuardarEnEEPROM();
         
       }
-
+      
       else if(datoMensaje.startsWith("*?", inicio) && datoMensaje.endsWith("*!")){
         String min = datoMensaje.substring(inicio + 2, fin);
         Serial.println("Received: " + min);
@@ -156,34 +164,45 @@ void updateSerial()
 
           GuardarEnEEPROM();
         }
-        
-        
-      }
 
+      }
       else if(datoMensaje.startsWith("*%?", inicio)){
         Serial.print("Ver datos en EEPROM");
         LecturaDeEEPROM();
 
+        //enviarMSNtxt("Joder Tio", Configuracion.NumeroTelefonicoUsuario);
+        
         String txt;
 
         txt = "Usuario Notificaciones: ";
         txt += Configuracion.NumeroTelefonicoUsuario;
         txt += "\n";
+
+        //enviarMSNtxt(txt, Configuracion.NumeroTelefonicoUsuario);
+        
         txt += "Canal Servidor: ";
         txt += Configuracion.NumeroTelefonicoServer;
         txt += "\n";
+        
+        //enviarMSNtxt(txt, Configuracion.NumeroTelefonicoUsuario);
+
         txt += "Nivel Minimo Sensor: "; 
         txt += Configuracion.levelMIN;
         txt += "\n";
+        
+        //enviarMSNtxt(txt, Configuracion.NumeroTelefonicoUsuario);
+        
         txt += "Nivel Maximo Sensor: ";
         txt += Configuracion.levelMAX;
         txt += "\n";
-
+        
         enviarMSNtxt(txt, Configuracion.NumeroTelefonicoUsuario);
+
 
       }
 
       datoMensaje="";
+    
 
   }
 
@@ -191,10 +210,15 @@ void updateSerial()
 
 void LecturaDeEEPROM(){
   Serial.println("Obteniendo datos de EPROM");
-  Settings MiObjetoResultado;
-  EEPROM.get(0, MiObjetoResultado);
+  //Settings MiObjetoResultado;
+  EEPROM.get(0, Configuracion);
   
-  Configuracion = MiObjetoResultado;
+  //Configuracion = MiObjetoResultado;
+
+  Serial.println(Configuracion.NumeroTelefonicoUsuario);
+  Serial.println(Configuracion.NumeroTelefonicoServer);
+  Serial.println(Configuracion.levelMIN);
+  Serial.println(Configuracion.levelMAX);
 }
 
 void GuardarEnEEPROM(){
@@ -204,14 +228,14 @@ void GuardarEnEEPROM(){
     Presets = Configuracion;
       
   
-    EEPROM.put(0,Presets);
+    EEPROM.put(0, Presets);
     Serial.print("Tamaño final del objeto = ");
     Serial.println(sizeof(Presets));
 }
 
 
-void enviarMSNtxt(String txt, String telefono){
-  String mensage = "AT+CMGS=\"" + telefono + "\"";
+void enviarMSNtxt(String txt, char telefono[]){
+  String mensage = "AT+CMGS=\"" + String(telefono) + "\"";
   mySerial.println(mensage);//change ZZ with country code and xxxxxxxxxxx with phone number to sms
   updateSerial();
 
